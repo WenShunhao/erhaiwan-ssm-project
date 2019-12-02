e4d2181862bfd25c2af215ab90b865e26d4b174c
wen shunhao <923134159@qq.com>
2019-12-02 20:01:15 +0800

    first commit

--- erhaiwan-ssm-project-entity/src/main/java/com/entity/OperatorInfo.java
+++ erhaiwan-ssm-project-entity/src/main/java/com/entity/OperatorInfo.java
@@ -1,4 +1,13 @@
 package com.entity;
 
+import lombok.Data;
+
+@Data//lombok -->优化pojo类代码块
 public class OperatorInfo {
+    //OperatorInfo类的依次各字段：编号、账号、密码、账号类型、员工编号
+    private Integer oId;
+    private String oAccount;
+    private String oPassword;
+    private String oType;
+    private Integer eId;
 }
