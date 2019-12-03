6156c4c59fa84a77d54a54d26042ed1488e4e46a
wen shunhao <923134159@qq.com>
2019-12-03 08:55:22 +0800

    first commit

--- erhaiwan-ssm-project-web/src/main/java/com/control/LoginControl.java
+++ erhaiwan-ssm-project-web/src/main/java/com/control/LoginControl.java
@@ -2,11 +2,14 @@ package com.control;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginControl {
-    @RequestMapping("/")
-    public String indexlogin(){
-        return "adminLogin";
+    @RequestMapping("/login")
+    public ModelAndView indexlogin(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("adminLogin");
+        return mav;
     }
 }
